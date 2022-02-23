4a124e88a9d86cc495fb08ae68ab2bb97656d94f
xiaoyudpp <xc184@duke.edu>
2022-02-23 17:02:29 -0500

    add 2 class

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -1,100 +1,102 @@
-# %% [markdown]
-# # Computational complexity/Data structures
+class Patient(object):
+    """Patient information'"""
+
+    def __init__(self, ID, GENDER, DOB, RACE):
+        self.ID = ID
+        self.DOB = DOB
+        self.GENDER = GENDER
+        self.RACE = RACE
+
+    def age(self) -> float:
+        return 2022 - int(self.DOB.split()[0].split("-")[0])
 
 
-# %% [markdown]
-# ## Data parsing
+class Lab(object):
+    """Patient's lab information"""
 
-# %%
-def parse_data(path_to_file: str) -> list[list[str]]:
-    "read file.txt to a list line by line and split by tab\t"
+    def __init__(self, ID, Labname, Value, Units, Labtime):
+        self.ID = ID
+        self.Value = Value
+        self.Labname = Labname
+        self.Units = Units
+        self, Labtime = Labtime
+
+
+def parse_data_patient(path_to_file: str) -> dict[str, Patient]:
+    "read patient.txt to a list line by line and split by tab\t"
     "computational complexity :0(N)"
     with open(path_to_file) as file:
         lines = file.readlines()
-    data = []
-    for i in lines:  # N times
-        data.append(i.split("\t"))  # O(1)
+    data = dict()
+    for i, line in enumerate(lines):
+        if i > 0:
+            ID, GENDER, DOB, RACE, _, _, _ = line.split("\t")
+            data[ID] = Patient(ID, GENDER, DOB, RACE)
     return data
 
 
-# %% [markdown]
-# ## Analysis
+print(parse_data_patient("PatientCorePopulatedTable.txt"))
 
 
-# %% [markdown]
-# ### Old patients
+def parse_data_lab(path_to_file: str) -> dict[str, Lab]:
+    "read labs.txt to a list line by line and split by tab\t"
+    "computational complexity :0(N)"
+    with open(path_to_file) as file:
+        lines = file.readlines()
+    data = dict()
+    for i, line in enumerate(lines):
+        if i > 0:
+            ID, _, Labname, Value, Units, Labtime = line.split("\t")
+            data[ID] = Lab(ID, Labname, Value, Units, Labtime)
+    return data
 
-# %%
-def age(a: str) -> int:
-    """change format of date and transfer into ages(compare with 2022)"""
-    return 2022 - int(a.split()[0].split("-")[0])
 
-
-# %%
-def num_older_than(age_over: int, data: list[list[str]]) -> int:
-    """compare ages line by line in data with chosen number(age_over),and return the total number of patients"
+def num_older_than(age_over: int, data: dict[str, Patient]) -> int:
+    """Calculate number of patients older than a given age"
     computational complexity :0(N)"""
     number_of_patient = 0
-    for i, line in enumerate(data):  # N times
-        if i > 0:  # O(1)
-            if age(line[2]) >= age_over:  # O(1)
-                number_of_patient += 1  # O(1)
+    for patient in data.values():
+        if patient.age() >= age_over:
+            number_of_patient += 1
     return number_of_patient
 
 
-# %% [markdown]
-# ### Sick patients
-
-# %%
 def sick_patients(
-    lab: str, gt_lt: str, value: float, data: list[list[str]]
+    lab: str, gt_lt: str, value: float, data: dict[str, Lab]
 ) -> list[str]:
     """find sick patients in data confirmed by Three factors(lab, gt_lt, value)
     computational complexity :0(N)"""
-    df = []
-    if gt_lt == ">":  # O(1)
-        for i, line in enumerate(data):  # N times
-            if i > 0:  # O(1)
-                if line[2] == lab:  # O(1)
-                    if float(line[-3]) > value:  # O(1)
-                        df.append(line[0])  # O(1)
-    if gt_lt == "<":  # O(1)
-        for i, line in enumerate(data):  # N times
-            if i > 0:  # O(1)
-                if line[2] == lab:  # O(1)
-                    if float(line[-3]) < value:  # O(1)
-                        df.append(line[0])  # O(1)
+    sick_patients = set()
+    if gt_lt == ">":
+        for patient in data.values():
+            if patient.Labname == lab:
+                if patient.Value > value:
+                    sick_patients.add(patient.ID)
+    if gt_lt == "<":
+        for patient in data.values():
+            if patient.Labname == lab:
+                if patient.Value > value:
+                    sick_patients.add(patient.ID)
     else:
         raise ValueError("gt_lt can only be choosen from > or <")
-    return df
+    return sick_patients
 
 
-# %% [markdown]
-# ### Age of patients when admission
-
-
-def year(Date: str) -> int:
-    """change format of date only keep year"""
-    return int(Date.split()[0].split("-")[0])
-
-
-def age_admission(patientid: str) -> int:
+def age_admission(
+    patientid: str, data1: dict[str, Lab], data2: dict[str, Patient]
+) -> int:
     """compare the birth year from patient*file with the earilst record year from lab*file"""
-    df1 = parse_data("LabsCorePopulatedTable.txt")
-    df2 = parse_data("PatientCorePopulatedTable.txt")
-    test_date = []
-    for i, line in enumerate(df1):
-        if i > 0:
-            if line[0] == "patientid":
-                test_date.append(year(line[-1]))
-            else:
-                raise ValueError("patientid can not be found in df1")
-    for i, line in enumerate(df2):
-        if i > 0:
-            if line[0] == "1A8791E3-A61C-455A-8DEE-763EB90C9B2C":
-                birthyear = year(line[2])
-            else:
-                raise ValueError("patientid can not be found in df2")
-    test_date.sort()
-    age_at = test_date[0] - birthyear
-    return age_at
+    data = 2022
+    if patientid in data1:
+        for patient in data1:
+            if data1.Labtime < data:
+                date = data1.Labtime
+    else:
+        raise PatientError("Patient is not in lab data.")
+    if patientid in data2:
+        for patient in data2:
+            birthyear = int(patient.DOB.split()[0].split("-")[0])
+    else:
+        raise PatientError("Patient is not in patient data.")
+    age_at_admission = date - birthyear
+    return age_at_admission
