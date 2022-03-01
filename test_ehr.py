d0f10e9fa8a867a67ee7a80720a05de981c67349
Echo chen <90811429+xiaoyudpp@users.noreply.github.com>
2022-03-01 18:15:03 -0500

    changed to small test data files

--- test_ehr.py
+++ test_ehr.py
@@ -3,35 +3,36 @@ import ehr_analysis as ehr
 
 def test_parse_data():
 
-    result = ehr.parse_data("LabsCorePopulatedTable.txt")
-    assert len(result) == 111483
+    result = ehr.parse_data("data/test_patient_data.txt")
+    assert len(result) == 10
 
 
 def test_num_older_than():
 
-    result = ehr.num_older_than(60, ehr.parse_data("PatientCorePopulatedTable.txt"))
+    result = ehr.num_older_than(60, ehr.parse_data("data/test_patient_data.txt"))
     expected = 58
     assert result == expected
 
 
 def test_sick_patients():
-
+    data = ehr.parse_data("data/test_lab_data.txt")
     result = ehr.sick_patients(
         "URINALYSIS: RED BLOOD CELLS",
         ">",
         1,
-        ehr.parse_data("LabsCorePopulatedTable.txt"),
+        data,
     )
     expected = 1
     assert len(result) == expected
 
 
 def test_age_admission():
-
+    data_patient = ehr.parse_data("data/test_patient_data.txt")
+    data_lab = ehr.parse_data("data/test_lab_data.txt")
     result = ehr.age_admission(
         "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-        ehr.parse_data("LabsCorePopulatedTable.txt"),
-        ehr.parse_data("PatientCorePopulatedTable.txt"),
+        data_patient,
+        data_lab,
     )
     expected = 18.9
     assert result == expected
