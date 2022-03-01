25c0934e2c011bbda2da5f9a8dfe3c7e1719155a
Echo chen <90811429+xiaoyudpp@users.noreply.github.com>
2022-03-01 18:04:46 -0500

    Changing names for variables & adding types hint

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -2,27 +2,31 @@ from datetime import datetime
 class Patient(object):
     """Patient information'"""
 
-    def __init__(self, ID, GENDER, DOB, RACE):
-        self.ID = ID
-        self.DOB = datetime.fromisoformat(DOB)
-        self.GENDER = GENDER
-        self.RACE = RACE
+    def __init__(self, id:str, gender:str, dob:str, race:str):
+        """declare variables in the class patient"""
+        self.id = id
+        self.dob = datetime.fromisoformat(dob)
+        self.gender = gender
+        self.race = race
+
     @property
     def age(self) -> float:
+        """return patient's age """
         today = datetime.now()
-        current_age = today - self.DOB
+        current_age = today - self.dob
         return current_age.days / 365.25
 
 
 class Lab(object):
     """Patient's lab information"""
 
-    def __init__(self, id, Labname, Value, Units, Labtime):
-        self.ID = id
-        self.Labname = Labname
-        self.Value = Value
-        self.Units = Units
-        self.Labtime = datetime.fromisoformat(Labtime)
+    def __init__(self, patientid:str, name:str, value:str, units:str, time:str):
+        """declare variables in the class lab"""
+        self.patientid = patientid
+        self.name = name
+        self.value = value
+        self.units = units
+        self.time = datetime.fromisoformat(time)
 
 
 def parse_data(path_to_file: str) -> list[Lab] or list[Patient] :
@@ -34,11 +38,11 @@ def parse_data(path_to_file: str) -> list[Lab] or list[Patient] :
     for i, line in enumerate(lines):
         if i > 0:
             if path_to_file=='LabsCorePopulatedTable.txt':
-                id, _, Labname, Value, Units, Labtime = line.strip("\n").split("\t")
-                data.append(Lab(id, Labname, Value, Units, Labtime))
+                patientid, _, name, value, units, time = line.strip("\n").split("\t")
+                data.append(Lab(patientid, name, value, units, time))
             if path_to_file=='PatientCorePopulatedTable.txt':
-                ID, GENDER, DOB, RACE, _, _, _ = line.strip('\n').split("\t")
-                data.append(Patient(ID, GENDER, DOB, RACE))
+                id, gender, dob, race, _, _, _ = line.strip('\n').split("\t")
+                data.append(Patient(id, gender, dob, race))
     return data
 
 
@@ -52,20 +56,20 @@ def num_older_than(age_over: int, data: list[Patient]) -> int:
     return number_of_patient
 
 
-def sick_patients(lab: str, gt_lt: str, value: float, data: list[Lab]) -> list[str]:
+def sick_patients(labname: str, gt_lt: str, value: float, data: list[Lab]) -> list[str]:
     """find sick patients in data confirmed by Three factors(lab, gt_lt, value)
     computational complexity :O(N*M)"""
     sick_patients = set()
     if gt_lt == ">":
         for lab in data:
-            if lab.Labname == lab:
-                if float(lab.Value) > value:
-                    sick_patients.add(lab.ID)
+            if lab.name == labname:
+                if float(lab.value) > value:
+                    sick_patients.add(lab.patientid)
     if gt_lt == "<":
         for lab in data:
-            if lab.Labname == lab:
-                if float(lab.Value) > value:
-                    sick_patients.add(lab.ID)
+            if lab.name == labname:
+                if float(lab.value) > value:
+                    sick_patients.add(lab.patientid)
     else:
         raise ValueError("gt_lt can only be choosen from > or <")
     return list(sick_patients)
@@ -78,15 +82,15 @@ def age_admission(
     The computational complexity of this function is O(N)"""
     date = datetime.now()
     for lab in data_lab:
-        if lab.ID == patientid:
-            if lab.Labtime < date:
-                date = lab.Labtime
+        if lab.patientid == patientid:
+            if lab.time < date:
+                date = lab.time
         else:
             raise ValueError("Patient is not in data.")
 
     for patient in data_patient:
-        if patient.ID == patientid:
-            birthday = patient.DOB
+        if patient.id == patientid:
+            birthday = patient.dob
         else:
             raise ValueError("Patient is not in data.")
     age_at_admission = (date - birthday).days/365.25
