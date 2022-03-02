02b1977224f59ada662e151627e0a365c84c1be7
Echo chen <90811429+xiaoyudpp@users.noreply.github.com>
2022-03-02 12:10:14 -0500

    formatter black

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
+
 class Patient(object):
     """Patient information'"""
 
-    def __init__(self, id:str, gender:str, dob:str, race:str):
+    def __init__(self, id: str, gender: str, dob: str, race: str):
         """declare variables in the class patient"""
         self.id = id
         self.dob = datetime.fromisoformat(dob)
@@ -11,7 +13,7 @@ class Patient(object):
 
     @property
     def age(self) -> float:
-        """return patient's age """
+        """return patient's age"""
         today = datetime.now()
         current_age = today - self.dob
         return current_age.days / 365.25
@@ -20,7 +22,7 @@ class Patient(object):
 class Lab(object):
     """Patient's lab information"""
 
-    def __init__(self, patientid:str, name:str, value:str, units:str, time:str):
+    def __init__(self, patientid: str, name: str, value: str, units: str, time: str):
         """declare variables in the class lab"""
         self.patientid = patientid
         self.name = name
@@ -29,7 +31,7 @@ class Lab(object):
         self.time = datetime.fromisoformat(time)
 
 
-def parse_data(path_to_file: str) -> list[Lab] or list[Patient] :
+def parse_data(path_to_file: str) -> list[Lab] or list[Patient]:
     "read labs.txt to a list line by line and split by tab\t"
     "computational complexity :O(N*M)"
     with open(path_to_file) as file:
@@ -37,11 +39,11 @@ def parse_data(path_to_file: str) -> list[Lab] or list[Patient] :
     data = []
     for i, line in enumerate(lines):
         if i > 0:
-            if path_to_file=='LabsCorePopulatedTable.txt':
+            if path_to_file == "LabsCorePopulatedTable.txt":
                 patientid, _, name, value, units, time = line.strip("\n").split("\t")
                 data.append(Lab(patientid, name, value, units, time))
-            if path_to_file=='PatientCorePopulatedTable.txt':
-                id, gender, dob, race, _, _, _ = line.strip('\n').split("\t")
+            if path_to_file == "PatientCorePopulatedTable.txt":
+                id, gender, dob, race, _, _, _ = line.strip("\n").split("\t")
                 data.append(Patient(id, gender, dob, race))
     return data
 
@@ -77,7 +79,7 @@ def sick_patients(labname: str, gt_lt: str, value: float, data: list[Lab]) -> li
 
 def age_admission(
     patientid: str, data_lab: list[Lab], data_patient: list[Patient]
-) -> int:
+) -> float:
     """compare the birth year from patient*file with the earilst record year from lab*file
     The computational complexity of this function is O(N)"""
     date = datetime.now()
@@ -86,13 +88,12 @@ def age_admission(
             if lab.time < date:
                 date = lab.time
         else:
-            raise ValueError("Patient is not in data.")
+            raise ValueError("Patient is not in lab data.")
 
     for patient in data_patient:
         if patient.id == patientid:
             birthday = patient.dob
         else:
-            raise ValueError("Patient is not in data.")
-    age_at_admission = (date - birthday).days/365.25
-    return age_at_admission
-    
+            raise ValueError("Patient is not in patient data.")
+    age_at_admission = (date - birthday).days / 365.25
+    return round(age_at_admission, 1)
--- test_ehr.py
+++ test_ehr.py
@@ -3,32 +3,32 @@ import ehr_analysis as ehr
 
 def test_parse_data():
 
-    result = ehr.parse_data("data/test_patient_data.txt")
+    result = ehr.parse_data("PatientCorePopulatedTable.txt")
     assert len(result) == 10
 
 
 def test_num_older_than():
 
-    result = ehr.num_older_than(60, ehr.parse_data("data/test_patient_data.txt"))
-    expected = 58
+    result = ehr.num_older_than(60, ehr.parse_data("PatientCorePopulatedTable.txt"))
+    expected = 6
     assert result == expected
 
 
 def test_sick_patients():
-    data = ehr.parse_data("data/test_lab_data.txt")
+    data = ehr.parse_data("LabsCorePopulatedTable.txt")
     result = ehr.sick_patients(
         "URINALYSIS: RED BLOOD CELLS",
-        ">",
+        "<",
         1,
         data,
     )
-    expected = 1
+    expected = ["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"]
     assert len(result) == expected
 
 
 def test_age_admission():
-    data_patient = ehr.parse_data("data/test_patient_data.txt")
-    data_lab = ehr.parse_data("data/test_lab_data.txt")
+    data_patient = ehr.parse_data("PatientCorePopulatedTable.txt")
+    data_lab = ehr.parse_data("LabsCorePopulatedTable.txt")
     result = ehr.age_admission(
         "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
         data_patient,
