50029bbeadeb08118a5ca41d831a2a2b2c20c7da
Echo chen <90811429+xiaoyudpp@users.noreply.github.com>
2022-04-16 12:46:21 -0400

    remove database

--- test_ehr.py
+++ test_ehr.py
@@ -1,20 +1,22 @@
 import ehr_analysis as ehr
+import os
 
 
 def test_parse_data():
-
+    os.remove("new_db.db")
     result = ehr.parse_data("PatientCorePopulatedTable.txt")
     assert len(result) == 10
 
 
 def test_num_older_than():
-
+    os.remove("new_db.db")
     result = ehr.num_older_than(60, ehr.parse_data("PatientCorePopulatedTable.txt"))
     expected = 6
     assert result == expected
 
 
 def test_sick_patients():
+    os.remove("new_db.db")
     data = ehr.parse_data("LabsCorePopulatedTable.txt")
     result = ehr.sick_patients(
         "URINALYSIS: RED BLOOD CELLS",
@@ -26,8 +28,10 @@ def test_sick_patients():
 
 
 def test_age_admission():
+    os.remove("new_db.db")
     data_patient = ehr.parse_data("PatientCorePopulatedTable.txt")
     data_lab = ehr.parse_data("LabsCorePopulatedTable.txt")
+
     result = ehr.age_admission(
         "1A8791E3-A61C-455A-8DEE-763EB90C9B2C", data_lab, data_patient
     )
